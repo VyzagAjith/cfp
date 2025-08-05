d1403749bdef8f46dd19e12eab12f9b4504438e2
VYZAG <101535334+VyzagAjith@users.noreply.github.com>
2025-08-05 13:24:47 +0530

    Update lexer.c
    It is able to get the function from lexer.h and it is defined here and is working perfectly. The compiler is now able to get the file, pipe it to the lexer and let the lexer read the file contents.

--- compiler/lexer.c
+++ compiler/lexer.c
@@ -1,95 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <ctype.h>
+#include "lexer.h"
+void LEX_source_code(const char *source)
+    {
+	FILE *file = fopen(source, "r");
+	
+	if (!file){
+	    perror("No file with the name given");
+	    exit(1);
+	}
+	else if (file == NULL){
+	    printf("Nothing in %s to be read", source);
+	    exit(2);
+	}
+	else 
+	    printf("Reading the file: %s\n", source);
+	
+	int i;
+	while ((i = fgetc(file)) != EOF){
+	    putchar(i);
+	}
+	fclose(file);
+    }
 
-//Keywords 
-typedef enum {
-    TOK_NUM,
-    TOK_CHAR,
-    TOK_STRING,
-    TOK_FLOAT,
-    TOK_DOUBLE,
-    TOK_FOR,
-    TOK_WHILE,
-    TOK_IF,
-    TOK_ELSE,
-    TOK_ELSEIF,
-    TOK_FUNC,
-    TOK_VOID,
-    TOK_GLOBAL,
-    TOK_WINDOW,
-    TOK_MAIN,
-    TOK_RETURN,
-    TOK_TYPEDEF,
-}KEYWORD_TOKS;
-
-//Identifiers
-typedef enum {
-    TOK_VARIABLE,
-    TOK_ENUM_VARIABLE,
-}IDENTIFIER_TOKS;
-
-//Constants
-typedef enum {
-    TOK_CONST,
-    TOK_DEFINE,
-}CONSTANT_TOKS;
-
-//Operators
-typedef enum {
-    TOK_ADD,
-    TOK_SUBTRACT,
-    TOK_MULTIPLY,
-    TOK_DIVIDE,
-    TOK_MOD,
-    TOK_EXPONENT,
-    TOK_POWER_OF_10,
-    TOK_POWER_OF_e,
-    TOK_NATURAL_LOG,
-    TOK_LOG,
-    TOK_SINE,
-    TOK_COSINE,
-    TOK_TAN,
-    TOK_SEC,
-    TOK_COSEC,
-    TOK_COT,
-    //TOK_SINE_INVERSE,
-    //TOK_COS_INVERSE,
-    //TOK_TAN_INVERSE,
-    //TOK_SEC_INVERSE,
-    //TOK_COSEC_INVERSE,
-    //TOK_COT_INVERSE,
-    TOK_MATRIX_ADDITION,
-    TOK_MATRIX_SUBTRACTION,
-    TOK_MATRIX_MULTIPLICATION,
-    TOK_MATRIX_DIVISION,
-    TOK_EIGEN_VALUES,
-    TOK_EIGEN_VECTORS,
-    TOK_NORMALIZE,
-    TOK_DIFFERENTIATE,
-    TOK_INTEGRATE,
-    TOK_SOLVE_QUADRATIC,
-    TOK_SOLVE_CUBIC,
-    TOK_SOLVE, //solves equation of any degree mentioned
-    TOK_SOLVE_DIFFERENTIAL,
-    TOK_SOLVE_INTEGRAL,
-}OPERATOR_TOKS;
-
-typedef enum {
-    TOK_LBRACKET,
-    TOK_RBRACKET,
-    TOK_COMMA,
-    TOK_OPEN_PARANTHESIS,
-    TOK_CLOSED_PARANTHESIS,
-    TOK_LBRACES,
-    TOK_RBRACES,
-    TOK_SEMICOLON,
-    TOK_COLON,
-}SYMBOLS_TOKS;
-
-int main()
-{
-    printf("Compiled and running well\n");
-
-}
