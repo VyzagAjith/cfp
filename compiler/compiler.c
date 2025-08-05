4daf360da838ca6393b23bc0d3d995f9f9f1bbcf
VYZAG <101535334+VyzagAjith@users.noreply.github.com>
2025-08-05 20:46:59 +0530

    Update compiler.c

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -5,11 +5,11 @@
 int main(int argc,char *argv[]) 
 {
     if (argc<2){
-	fprintf(stderr,"Do %s <file_name>\n", argv[0]);
+	fprintf(stderr,"ERROR: Do %s <file_name>\n", argv[0]);
 	return(1);
     }
     
-    LEX_source_code(argv[1]);
+TOKENIZE(argv[1]);
 
 }
 
