58cf52e95ef37b6df38139b1a27d009c75ccdc92
VYZAG <101535334+VyzagAjith@users.noreply.github.com>
2025-09-02 07:06:25 +0530

    Only accepts .cfp files

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lexer.h"
-    
+#include <string.h>
+
+
 int main(int argc,char *argv[]) 
 {
     if (argc<2){
@@ -9,7 +11,14 @@ int main(int argc,char *argv[])
 	return(1);
     }
     
-TOKENIZE(argv[1]);
+    //to check if the filename ends with .cfp
+    char *ext = strrchr(argv[1], '.'); 
+    
+    if (!ext || strcmp(ext , ".cfp") != 0){
+	     fprintf(stderr, "ERROR: %s Wrong file type. File extension must be '.cfp'\n", argv[1]);
+    }
 
+    else{ 
+	TOKENIZE(argv[1]);
+    }
 }
-
