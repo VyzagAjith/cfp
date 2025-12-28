2ad70d7502970d1e9e3041e556851c23d6b49c95
VyzagAjith <vyzagajith2k19@gmail.com>
2025-12-28 15:03:45 +0530

    Added --help and -version comands

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -7,18 +7,30 @@
 int main(int argc,char *argv[]) 
 {
     if (argc<2){
-	fprintf(stderr,"ERROR: Do %s <file_name>\n", argv[0]);
+	fprintf(stderr,"ERROR: Do %s <file_name> |or --help to know how to use cfp\n", argv[0]);
 	return(1);
     }
-    
+    else if (strcmp(argv[1], "--help") == 0) {
+        printf("\tcfp - C For Physics\n");
+	printf("Syntax - cfp [filename.cfp]");
+        printf("\nOptions:\n");
+        printf("  --help       Show this help message\n");
+        printf("  --version    Show compiler version\n");
+        return 0;
+    }
+    if(strcmp(argv[1], "--version") == 0){
+	fprintf(stdout, "0.0.0 - In Development\n");
+	return(0);
+    }
     //to check if the filename ends with .cfp
     char *ext = strrchr(argv[1], '.'); 
     
     if (!ext || strcmp(ext , ".cfp") != 0){
-	     fprintf(stderr, "ERROR: %s Wrong file type. File extension must be '.cfp'\n", argv[1]);
+	fprintf(stderr, "ERROR: %s Wrong file type. File extension must be '.cfp'\nDo --help to learn the commands\n", argv[1]);
     }
-
+    
     else{ 
 	TOKENIZE(argv[1]);
     }
 }
+
